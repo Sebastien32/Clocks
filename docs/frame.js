ebd4e90044a5c8cd535e67a24db6d1737cd2497f
Sebastien32 <sbw0302@gmail.com>
2017-05-28 21:15:50 -0700

    Update frame.js

--- docs/frame.js
+++ docs/frame.js
@@ -1,21 +1,4 @@
-//lets display the current time
-/*var date;
-var hours;
-var minutes;
-var seconds;
-var milliseconds;
-var colorhex;
-var colortext;
-var red;
-var hexRed;
-var green;
-var hexGreen;
-var blue;
-var hexBlue;
-function displayTime()
-{
-    // Create a new date object
-    date = new Date();
+/*
     // Set the hours variable to the current number of hours
     hours = d.getHours();
     // Set the minutes variable to the current number of minutes
@@ -74,27 +57,49 @@ var green;
 var hexGreen;
 var blue;
 var hexBlue;
-function displayTime() {
-    date = new Date(); //new data object
+function displayTime()
+{
+    // Create a new date object
+    date = new Date();
+    // Set the hours variable to the current number of hours
     hours = date.getHours();
+    // Set the minutes variable to the current number of minutes
     minutes = date.getMinutes();
+    // Set the second variable to the current number of seconds
     seconds = date.getSeconds();
+    // Set the milliseconds variable to the current number of milliseconds
     milliseconds = date.getMilliseconds();
-    //add zero to the left of the numbers if they are single digits
+    // Add a zero to the left of numbers for formatiing
     if(hours <= 9) hours = '0' + hours;
     if(minutes <= 9) minutes = '0' + minutes;
     if(seconds <= 9) seconds = '0' + seconds;
-    if(milliseconds <= 9) milliseconds = '0' + milliseconds;
-    if(milliseconds <= 99) milliseconds = '0' + milliseconds;
-    colorhex = "#" + hours + minutes + seconds;
-    colortext = hours + ":" + minutes + ":" + seconds + "." + milliseconds;
+    // Set colortext variable to the display text
+    colortext = hours + ":" + minutes + ":" + seconds;
+    // Set the red value to the current proportion of a day passed
+    red = milliseconds + (1000 * seconds) + (60000 * minutes) + (3600000 * hours) / (86400000) * 256;
+    red = round(red);
+    // Set the green value to the current proportion of an hour passed
+    green = milliseconds + (1000 * seconds) + (60000 * minutes) / (3600000) * 256;
+    green = round(green);
+    // Set the blue value to the current proportion of a minute passed
+    blue = milliseconds + (1000 * seconds) / (60000) * 256;
+    blue = round(blue);
+    // Convert RBG values to hexadecimal
+    hexRed = red.toString(16);
+    hexGreen = green.toString(16);
+    hexBlue = blue.toString(16);
+    //set background color
+    if(red <= 15) hexRed = '0' + hexRed;
+    if(green <= 15) hexGreen = '0' + hexGreen;
+    if(blue <= 15) hexBlue = '0' + hexBlue;
+    colorhex = "#" + hexRed + hexGreen + hexBlue;
     //set background color
     document.body.style.background = colorhex;
     //set time
     document.getElementById("hex").innerHTML = colortext;
 
     //retrigger the function every second
-    setTimeout(displayTime, 100);
+    setTimeout(displayTime, 1000);
 }
 //call the function
 displayTime();
