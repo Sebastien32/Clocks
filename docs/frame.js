5980175ea5fb53d09d0eb4529748f7052b6be73b
Sebastien32 <sbw0302@gmail.com>
2017-05-28 18:35:20 -0700

    Update frame.js

--- docs/frame.js
+++ docs/frame.js
@@ -1,5 +1,9 @@
 //lets display the current time
-var d, h, m, s, color;
+var d;
+var h;
+var m;
+var s;
+var color;
 function displayTime() {
     d = new Date(); //new data object
     h = d.getHours();
@@ -7,11 +11,11 @@ function displayTime() {
     s = d.getSeconds();
 
     //add zero to the left of the numbers if they are single digits
-    if(h <= 9) h = '0'+h;
-    if(m <= 9) m = '0'+m;
-    if(s <= 9) s = '0'+s;
+    if(h <= 9) h = '0' + h;
+    if(m <= 9) m = '0' + m;
+    if(s <= 9) s = '0' + s;
 
-    color = "#"+h+m+s;
+    color = h + ":" + m + ":" + s;
     //set background color
     document.body.style.background = color;
     //set time
@@ -20,6 +24,5 @@ function displayTime() {
     //retrigger the function every second
     setTimeout(displayTime, 1000);
 }
-
 //call the function
 displayTime();
