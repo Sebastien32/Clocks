43a4602217dcdf6e77ecbbd0980ebb24f27943d2
Sebastien32 <sbw0302@gmail.com>
2017-05-28 19:34:30 -0700

    Update frame.js

--- docs/frame.js
+++ docs/frame.js
@@ -1,5 +1,5 @@
 //lets display the current time
-var date;
+/*var date;
 var hours;
 var minutes;
 var seconds;
@@ -54,6 +54,46 @@ function displayTime()
     //retrigger the function every millisecond
     setTimeout(displayTime, 1);
     hexRed = yourNumber.toString(16);
+}
+//call the function
+displayTime();
+*/
+
+//lets display the current time
+//lets display the current time
+var date;
+var hours;
+var minutes;
+var seconds;
+var milliseconds;
+var colorhex;
+var colortext;
+var red;
+var hexRed;
+var green;
+var hexGreen;
+var blue;
+var hexBlue;
+function displayTime() {
+    d = new Date(); //new data object
+    h = d.getHours();
+    m = d.getMinutes();
+    s = d.getSeconds();
+
+    //add zero to the left of the numbers if they are single digits
+    if(h <= 9) h = '0' + h;
+    if(m <= 9) m = '0' + m;
+    if(s <= 9) s = '0' + s;
+
+    colorhex = "#" + h + m + s;
+    colortext = h + ":" + m + ":" + s;
+    //set background color
+    document.body.style.background = colorhex;
+    //set time
+    document.getElementById("hex").innerHTML = colortext;
+
+    //retrigger the function every second
+    setTimeout(displayTime, 1000);
 }
 //call the function
 displayTime();
